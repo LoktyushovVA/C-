b4f73290112e6daa23eff87ec44599d8343ec820
LoktyushovVA <48481076+LoktyushovVA@users.noreply.github.com>
2021-10-02 17:14:30 +0300

    Add files via upload

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 from PyQt5.QtSql import QSqlDatabase
 from PyQt5.QtWidgets import QApplication, QMessageBox, QLabel
 import sys
-#!!!
+
 
 
 from psycopg2.extras import DictCursor
@@ -25,9 +25,12 @@ def openDatabase():
         print("[INFO] Error while connect db ", _ex)
         return None
 
+
+
+
 def GetNir():
     with dbc.dbcon.cursor() as cur:
-        query = f"select * from nir;"
+        query = f'select codvuz as "код вуза",rnw as "характер  НИР",z2 as "сокращенное наименование вуза",f6 as "руководитель НИР",f10 as "коды темы по ГРНТИ",f18 as "плановый объем финансирования",f2 as "наименование НИР",f7 as "должность руководителя" from nir;'
         # print(query)
         cur.execute(query)
         table = cur.fetchall()
@@ -35,7 +38,7 @@ def GetNir():
 
 def GetFin():
     with dbc.dbcon.cursor() as cur:
-        query = f"select codvuz as код_вуза,z2 as сокращенное_наименование,z3 as плановый_объем_финансирования,z18 as фактический_объем_финансирования,numworks as количество_НИР from f();"
+        query = f'select codvuz as "код вуза",z2 as "сокращенное наименование",z3 as "плановый объем финансирования",z18 as "фактический объем финансирования",numworks as "количество НИР" from f();'
         # print(query)
         cur.execute(query)
         table = cur.fetchall()
@@ -43,12 +46,12 @@ def GetFin():
 
 def GetRub():
     with dbc.dbcon.cursor() as cur:
-        query = f"select codrub as код_рубрики,rubrika as наименование_рубрики from grn order by 1;"
+        query = f'select codrub as "код рубрики",rubrika as "наименование рубрики" from grn order by 1;'
         # print(query)
         cur.execute(query)
         table = cur.fetchall()
         return table
-
+#soddfodfk
 def Getvuz():
     with dbc.dbcon.cursor() as cur:
         query = f"select * from vuz_for_rus;"
